1d79433e41c403aad3a9e1dbd87cf70ed1553c3f
sudomonikers <sudomonikers@gmail.com>
2024-10-14 17:52:15 -0400

    api

--- apis/restapi/api.go
+++ apis/restapi/api.go
@@ -4,40 +4,67 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 func main() {
-	ctx := context.Background()
-	// URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
-	dbUri := "neo4j://localhost"
-	dbUser := "neo4j"
-	dbPassword := "password"
-	dbBase := "neo4j"
-	driver, err := neo4j.NewDriverWithContext(
-		dbUri,
-		neo4j.BasicAuth(dbUser, dbPassword, ""))
-	if err != nil {
-		panic(err)
-	}
-	defer driver.Close(ctx)
 
-	err = driver.VerifyConnectivity(ctx)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Connection established.")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/match-all", func(c *gin.Context) {
 
-	result, err := neo4j.ExecuteQuery(
-		ctx,
-		driver,
-		"MATCH (n) RETURN n",
-		map[string]any{},
-		neo4j.EagerResultTransformer,
-		neo4j.ExecuteQueryWithDatabase(dbBase))
-	if err != nil {
-		fmt.Println("Error executing query:", err)
-		return
-	}
-	fmt.Println(result)
+		ctx := context.Background()
+		// URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+		dbUri := "neo4j://localhost"
+		dbUser := "neo4j"
+		dbPassword := "password"
+		dbBase := "neo4j"
+		driver, err := neo4j.NewDriverWithContext(
+			dbUri,
+			neo4j.BasicAuth(dbUser, dbPassword, ""))
+		if err != nil {
+			panic(err)
+		}
+		defer driver.Close(ctx)
+
+		err = driver.VerifyConnectivity(ctx)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Connection established.")
+
+		result, err := neo4j.ExecuteQuery(
+			ctx,
+			driver,
+			"MATCH (n) RETURN n",
+			map[string]any{},
+			neo4j.EagerResultTransformer,
+			neo4j.ExecuteQueryWithDatabase(dbBase))
+		if err != nil {
+			fmt.Println("Error executing query:", err)
+			return
+		}
+		// Iterate over the records and print the node properties
+		for _, record := range result.Records {
+			node, ok := record.Values[0].(neo4j.Node)
+			if !ok {
+				fmt.Println("Error casting to neo4j.Node")
+				continue
+			}
+			fmt.Printf("Node ID: %v\n", node.ElementId)
+			fmt.Printf("Labels: %v\n", node.Labels)
+			fmt.Printf("Properties: %v\n", node.Props)
+		}
+
+		c.JSON(200, gin.H{
+			"message": result.Records,
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+
 }
--- ui/atlas-of-us/src/app/knowledge-graph/knowledge-graph.component.ts
+++ ui/atlas-of-us/src/app/knowledge-graph/knowledge-graph.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
-import * as THREE from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer, CubeTextureLoader } from 'three';
+import {BoxGeometry, MeshBasicMaterial, Mesh, LineBasicMaterial, Vector3, BufferGeometry, Line} from 'three';
+import { OrbitControls } from 'three-stdlib';
+
+
+
+interface AnimatedMesh extends Mesh {
+  objectAnimation?: () => void;
+}
+
+
+
 
 @Component({
   selector: 'app-knowledge-graph',
@@ -10,29 +21,78 @@ import * as THREE from 'three';
   styleUrls: ['./knowledge-graph.component.scss'],
 })
 export class KnowledgeGraphComponent implements OnInit {
- ngOnInit(): void {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setAnimationLoop( animate );
-  document.body.appendChild( renderer.domElement );
-  
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-  
-  camera.position.z = 5;
-  
-  function animate() {
-  
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-  
-    renderer.render( scene, camera );
-  
+  scene: Scene;
+  camera: PerspectiveCamera;
+  renderer: WebGLRenderer;
+  controls: OrbitControls;
+  nodes: AnimatedMesh[] = [];
+
+  constructor() {
+    this.scene = new Scene();
+    this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.renderer = new WebGLRenderer({ antialias: true});
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.animate = this.animate.bind(this);
+
   }
- }
+
+  ngOnInit(): void {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+    this.controls.update();
+
+
+    // Load the skybox images
+    const loader = new CubeTextureLoader();
+    const texture = loader.load([
+      'px.png', // right
+      'nx.png', // left
+      'py.png', // top
+      'ny.png', // bottom
+      'pz.png', // front
+      'nz.png', // back
+    ]);
+    this.scene.background = texture;
+
+    this.camera.position.z = 1;
+
+    this.createNode(0, 0, 0); // Example: create a node at the origin
+    this.createNode(2, 2, 2); // Example: create a node at position (2, 2, 2)
+    this.createRelationship(this.nodes[0], this.nodes[1]); // Create a relationship between the two nodes
+
+    this.animate();
+    this.renderer.setAnimationLoop(this.animate);
+  }
+
+  animate() {
+    this.controls.update();
+    this.nodes.forEach((obj) => {
+      if (obj.objectAnimation) obj.objectAnimation();
+    })
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  createNode(x: number, y: number, z: number) {
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({ color: 0x00ff00 });
+    const cube: AnimatedMesh = new Mesh(geometry, material);
+    cube.position.set(x, y, z);
+    cube.objectAnimation = function() {
+      this.rotation.x += 0.01;
+      this.rotation.y += 0.01;
+    }
+    this.nodes.push(cube);
+    this.scene.add(cube);
+  }
+
+  createRelationship(from: AnimatedMesh, to: AnimatedMesh) {
+    const material = new LineBasicMaterial({ color: 0x0000ff });
+    const points = [];
+    points.push(new Vector3(from.position.x, from.position.y, from.position.z));
+    points.push(new Vector3(to.position.x, to.position.y, to.position.z));
+    const geometry = new BufferGeometry().setFromPoints(points);
+    const line = new Line(geometry, material);
+    this.scene.add(line);
+  }
 }
+
